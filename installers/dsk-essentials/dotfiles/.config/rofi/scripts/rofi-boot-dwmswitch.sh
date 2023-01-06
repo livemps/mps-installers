f03d0e6699421c3e4b256299dc77576369532c90
foo <foo@bar.baz>
2023-01-06 07:48:26 +0000

    Fix option handler in rofi

--- installers/dsk-essentials/dotfiles/.config/rofi/scripts/rofi-boot-dwmswitch.sh
+++ installers/dsk-essentials/dotfiles/.config/rofi/scripts/rofi-boot-dwmswitch.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
-
-THEME_FOLDER=/usr/share/rofi/themes
-THEME_NAME=rofi-boot-dwmswitch.rasi
-
-cat <<EOF | rofi -dmenu -theme $THEME_FOLDER/$THEME_NAME 
-[]= Tiled                        Layout	    0
-><> Floating                     Layout	    1
-[M] Monocle                      Layout	    2
-TTT BottomStack    (Vertical)    Layout	    3
-=== BottomStack    (Horizontal)  Layout	    4
-|M| CenteredMaster (Default)     Layout	    5
->M> CenteredMaster (Floating)    Layout	    6
-EOF
+THEMEFILE=/usr/share/rofi/themes/rofi-boot-dwmswitch.rasi
+{
+echo "[]= Tiled                        Layout	0"
+echo "><> Floating                     Layout	1"
+echo "[M] Monocle                      Layout	2"
+echo "TTT BottomStack    (Vertical)    Layout	3"
+echo "=== BottomStack    (Horizontal)  Layout	4"
+echo "|M| CenteredMaster (Default)     Layout	5"
+echo ">M> CenteredMaster (Floating)    Layout	6"
+} | cat |rofi -dmenu -theme $THEMEFILE | tail -c 2
