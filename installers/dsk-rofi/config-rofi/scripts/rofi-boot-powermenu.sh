731088e02901012a65212869c5f55e9bd84d9661
foo <foo@bar.baz>
2023-01-07 01:39:07 +0100

    Fix rofi powermenu

--- installers/dsk-rofi/config-rofi/scripts/rofi-boot-powermenu.sh
+++ installers/dsk-rofi/config-rofi/scripts/rofi-boot-powermenu.sh
@@ -3,7 +3,7 @@
 THEME_FOLDER=/usr/share/rofi/themes
 THEME_NAME=rofi-boot-powermenu.rasi
 
-SUDO_ASKPASS=$(which ssh-askpass)
+export SUDO_ASKPASS=$(which ssh-askpass)
 chosen=$(printf "Power Off\nRestart\nLogout\n" | rofi -dmenu -theme $THEME_FOLDER/$THEME_NAME)
 case "$chosen" in
         "Power Off")    sudo -A poweroff ;;
