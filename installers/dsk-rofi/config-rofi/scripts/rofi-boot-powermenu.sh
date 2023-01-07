f46658ce0c2766302d270c6dee8eacdaad2c4b65
foo <foo@bar.baz>
2023-01-07 01:01:50 +0000

    Add qt4-ssh-askpass

--- installers/dsk-rofi/config-rofi/scripts/rofi-boot-powermenu.sh
+++ installers/dsk-rofi/config-rofi/scripts/rofi-boot-powermenu.sh
@@ -3,7 +3,12 @@
 THEME_FOLDER=/usr/share/rofi/themes
 THEME_NAME=rofi-boot-powermenu.rasi
 
-export SUDO_ASKPASS=$(which ssh-askpass)
+DISTRO=$(lsb-release -si)
+if [ "$DISTRO" = "Arch" ] ; then
+    export SUDO_ASKPASS=$(which qt4-ssh-askpass)
+else
+    export SUDO_ASKPASS=$(which ssh-askpass)
+fi
 chosen=$(printf "Power Off\nRestart\nLogout\n" | rofi -dmenu -theme $THEME_FOLDER/$THEME_NAME)
 case "$chosen" in
         "Power Off")    sudo -A poweroff ;;
